f5c409b24f6b85dfa552041052aaf595b21d08b0
Nilly (iago) TheL0L <97253814+NillyTheL0L@users.noreply.github.com>
2022-03-25 12:48:42 -0300

    new apps

--- jewelrystore.rb
+++ jewelrystore.rb
@@ -11,8 +11,8 @@ if res == 'list'
     puts 'vscode'.colorize(:blue)
     puts 'emacs'.colorize(:purple)
     puts 'etcher'.colorize(:green)
-    puts 'brave'.colorize(:yellow)
-    puts 'stepmania'.colorize(:yellow)
+    puts 'brave'.colorize(:red)
+    puts 'stepmania'.colorize(:red)
     puts 'zoom'.colorize(:blue)
     puts 'schildichat'.colorize(:green)
     puts 'cpu-x'.colorize(:blue)
@@ -20,6 +20,13 @@ if res == 'list'
     puts 'blender'.colorize(:yellow)
     puts 'krita'.colorize(:pink)
     puts 'gimp'.colorize(:white)
+    puts 'vlc'.colorize(:yellow)
+    puts 'obs'.colorize(:red)
+    puts 'firefox'.colorize(:red)
+    puts 'waterfox'.colorize(:blue)
+    puts 'notepadqq'.colorize(:green)
+    puts 'libreoffice'.colorize(:yellow)
+    puts 'thunderbird'.colorize(:blue)
 
 end
 
@@ -159,6 +166,62 @@ if res == 'gimp'
 	puts 'I will install Gimp x86_64 AppImage, Correct? (Press Enter)'.colorize(:red)
 	gimpd = gets.chomp
 	puts 'Installing Gimp AppImage'.colorize(:red)
-	gimo = system( "cd ~/ && wget https://dl2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTYwMTkyNDUwNCwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiI4MGQ4OTZjNGEwOWJiZmJiYzNkZDRjZWRjZWUwNDFkN2RmMjIzODMzMWFiZTc4YTE2YTg2NWE4NzQxODc1OTcyNmQ3N2Y1ZGE3ZTdlMTM4ZGNiOGE5MTIyMmQ0YWJkZmUyNmRhYjk5M2M3MWI2ODE3OGYxZTYxODhhNjQ1YmYzYiIsInQiOjE2NDc5OTI2NjEsInN0ZnAiOiJhNzhmZmFhZTdkNDk3MDk2Y2ZhNjJmOTBmMmJiNGFmNyIsInN0aXAiOiIyMDEuMjE2Ljc0LjIyMyJ9.CpkSnEc13pPiDg93ayIBOdJG87WSBLrJ8g10YqIcWME/GIMP_AppImage-git-2.10.21-20201001-withplugins-x86_64.AppImage" )
+	gimp = system( "cd ~/ && wget https://dl2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTYwMTkyNDUwNCwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiI4MGQ4OTZjNGEwOWJiZmJiYzNkZDRjZWRjZWUwNDFkN2RmMjIzODMzMWFiZTc4YTE2YTg2NWE4NzQxODc1OTcyNmQ3N2Y1ZGE3ZTdlMTM4ZGNiOGE5MTIyMmQ0YWJkZmUyNmRhYjk5M2M3MWI2ODE3OGYxZTYxODhhNjQ1YmYzYiIsInQiOjE2NDc5OTI2NjEsInN0ZnAiOiJhNzhmZmFhZTdkNDk3MDk2Y2ZhNjJmOTBmMmJiNGFmNyIsInN0aXAiOiIyMDEuMjE2Ljc0LjIyMyJ9.CpkSnEc13pPiDg93ayIBOdJG87WSBLrJ8g10YqIcWME/GIMP_AppImage-git-2.10.21-20201001-withplugins-x86_64.AppImage" )
+	puts 'app  installed with sucess in your home'.colorize(:red)
+end
+
+if res == 'vlc'
+	puts 'I will install VLC x86_64 AppImage, Correct? (Press Enter)'.colorize(:red)
+	vlcd = gets.chomp
+	puts 'Installing VLC AppImage'.colorize(:red)
+	vlc = system( "cd ~/ && wget https://dl2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTYwMjAxMjYxMiwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiIzZTEyODAzYjI5YzU5NTBlNjA1ODFjZmRiMjllMDBhZTJkMTBiYzYyOWVjOTdhZjUwNWQ5ZjM2ZmNkNDgzYjIxN2RhNmZkOGEwMmNhNzU1YTAyNDU3NjVmYWNhODlkNjdjYWNjYmVkNDkzZTdkMTQ0YWU0ZGE1ZmQwOWZiNWQ2MCIsInQiOjE2NDgyMjMzMDEsInN0ZnAiOiJiZWMwODFlNTQxYmUwN2Q3MDA2MjUzNjUxMTQ3MjVhYiIsInN0aXAiOiIyMDEuMjE2Ljc0LjIyMyJ9.vSGnJQl3ibt-pfAwzVRU73CC61VzdTOPQn9_BNv1l98/VLC_media_player-3.0.11.1-x86_64.AppImage" )
+	puts 'app  installed with sucess in your home'.colorize(:red)
+end
+
+if res == 'obs'
+	puts 'I will install OBS Studio x86_64 AppImage, Correct? (Press Enter)'.colorize(:red)
+	obsd = gets.chomp
+	puts 'Installing OBS Studio AppImage'.colorize(:red)
+	obs = system( "cd ~/ && wget https://dl1.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTU5NjcxMjM3MywidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiI0YWRlNWM0NmZhYjUxN2U0YzVlYWFlZjg4MGRkMzE2NDI5ZTA0OGQxYmExZDFhZjM0MWUyMzFhNWM0N2U4Nzk2MWNiNjViMmRkNWNiNWI0OTFkMTA4MjViMWVkNGYzZGE0NzRjNDE4NjdhNmRlNjVmMDYwODZhNmFlNWY3OTRmYyIsInQiOjE2NDgyMjU0NTcsInN0ZnAiOiJiZWMwODFlNTQxYmUwN2Q3MDA2MjUzNjUxMTQ3MjVhYiIsInN0aXAiOiIyMDEuMjE2Ljc0LjIyMyJ9.fPk-JwiinAiuPwuTJdHO_73fihLl-3lJLi4d4ZQx0jE/obs-studio-plus-25.0.8-x86_64.AppImage" )
+	puts 'app  installed with sucess in your home'.colorize(:red)
+end
+
+if res == 'firefox'
+	puts 'I will install Firefox 70 x86_64 AppImage, Correct? (Press Enter)'.colorize(:red)
+	firefoxd = gets.chomp
+	puts 'Installing Firefox AppImage'.colorize(:red)
+	firefox = system( "cd ~/ && wget https://dl2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTU4Njc4ODA5MSwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiJiMmE4N2NiOTc0YzczN2Q3MGM3OGYwZDIzYmNiMmM1YTIyNWQyOWY3YTlkMTQyNWY0M2E0NDY5NjVkODUxNjQ1NTQ5MzEzOWYyZDZiNDk0OWQ3ZmZjN2E2Mzc3ZTBkZjBmNDFiMjdiNTk1OWYwNGNhZWVhYWU3ODY5Mjc5MzM1NCIsInQiOjE2NDgyMjU1NDksInN0ZnAiOiJiZWMwODFlNTQxYmUwN2Q3MDA2MjUzNjUxMTQ3MjVhYiIsInN0aXAiOiIyMDEuMjE2Ljc0LjIyMyJ9.zhl7JZEFmFyfeWg5aJYVGufegmCMnqtSnrr0tu_CK2Q/Firefox-x86_64-20200413141725.AppImage" )
+	puts 'app  installed with sucess in your home'.colorize(:red)
+end
+
+if res == 'waterfox'
+	puts 'I will install Waterfox 50 x86_64 AppImage, Correct? (Press Enter)'.colorize(:red)
+	waterfoxd = gets.chomp
+	puts 'Installing Waterfox AppImage'.colorize(:red)
+	waterfox = system( "cd ~/ && wget https://dl2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTU0MjQ0OTA5NiwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiI5NWFkODE1NGUzNGQzZjgwYzVkYzFlMDZmYzZkNDVlMTkzMThjZDEyZDE4ODA4NWRmNmVmNWM0YzU5ODM3ODgwYmFlMzQ4ZTVjYzM4MTkwZWViNWM3NWVkZDJmODY5MGJiNDE1ODdiZjRkMTA3ZmZmYjJiNjA1ODYzOGY5NzE5OSIsInQiOjE2NDgyMjU2MTIsInN0ZnAiOiJiZWMwODFlNTQxYmUwN2Q3MDA2MjUzNjUxMTQ3MjVhYiIsInN0aXAiOiIyMDEuMjE2Ljc0LjIyMyJ9.VqghsXgzV_NYfdTTTKKbqCCNYD9whsomV8Q4FDIjUEQ/Waterfox-0-Buildlp150.4.1.glibc2.17-x86_64.AppImage" )
+	puts 'app  installed with sucess in your home'.colorize(:red)
+end
+
+if res == 'notepadqq'
+	puts 'I will install Notepadqq x86_64 AppImage, Correct? (Press Enter)'.colorize(:red)
+	notepadqqd = gets.chomp
+	puts 'Installing Notepadqq AppImage'.colorize(:red)
+	notepadqq = system( "cd ~/ && wget https://objects.githubusercontent.com/github-production-release-asset-2e65be/3536442/f1e00584-3548-11e8-98bb-27c7bacfd184?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20220325%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220325T152852Z&X-Amz-Expires=300&X-Amz-Signature=fb95bbe5774d7f64f9b26aad00fba76b3a6eb04a3f25f3576dd9e6b568fefdab&X-Amz-SignedHeaders=host&actor_id=97253814&key_id=0&repo_id=3536442&response-content-disposition=attachment%3B%20filename%3Dnotepadqq-.glibc2.14-x86_64.AppImage&response-content-type=application%2Foctet-stream" )
+	puts 'app  installed with sucess in your home'.colorize(:red)
+end
+
+if res == 'libreoffice'
+	puts 'I will install Libreoffice 7.1 x86_64 AppImage, Correct? (Press Enter)'.colorize(:red)
+	libreofficed = gets.chomp
+	puts 'Installing Libreoffice AppImage'.colorize(:red)
+	libreoffice = system( "cd ~/ && wget https://dl2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTYwMTk1MzM5MiwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiI5YmEwZWNjZGMzNWNkMDNiYWI3YmYyODA2NGIyN2NiNmY0MzZiOTg1MGQyNWQ5Y2JmYWZhY2Y3ZjA1M2YzOGU4MGVhNzMyYTIwNzk5MTQzNjBmMzUyYzNmY2E2MmE3NDA2NzRiZTM0MWJlNzA4OWI2NDdhNmZiYjg4OGRmMjJjOSIsInQiOjE2NDgyMjU3ODIsInN0ZnAiOiJiZWMwODFlNTQxYmUwN2Q3MDA2MjUzNjUxMTQ3MjVhYiIsInN0aXAiOiIyMDEuMjE2Ljc0LjIyMyJ9.jd6arR3X-PB_xOJCZqqthBXNq7bLTUu1_1OLtXdtdRg/LibreOffice-fresh.full.help-x86_64.AppImage" )
+	puts 'app  installed with sucess in your home'.colorize(:red)
+end
+
+if res == 'thunderbird'
+	puts 'I will install Mozilla Thunderbird x86_64 AppImage, Correct? (Press Enter)'.colorize(:red)
+	thunderbirdd = gets.chomp
+	puts 'Installing Thunderbird AppImage'.colorize(:red)
+	thunderbird = system( "cd ~/ && wget https://dl1.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTYwMjAxNzA3NCwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiI4MzRiNzdlNGViNmM3MTBhZmViMTk3M2M3MDU5MzRkMjZhZGM0YmE0MjdjYzY2YjcyYjU1YzZkODA3NzgwMzk3YmIzMDE3NmE0MGY4NzY3NTc2ODNjMGI4NWRiMDNmMWYxNzBjM2MzN2VhY2U3Nzk1MGMxNzI4ODY1ODlmNDRjZCIsInQiOjE2NDgyMjU4MTQsInN0ZnAiOiJiZWMwODFlNTQxYmUwN2Q3MDA2MjUzNjUxMTQ3MjVhYiIsInN0aXAiOiIyMDEuMjE2Ljc0LjIyMyJ9.Uyi_NA3tmdeJBmBIL_BvdL2Hlvt7ZOpdV4gsFt31suo/Thunderbird-78.3_20200925001233.AppImage" )
 	puts 'app  installed with sucess in your home'.colorize(:red)
 end
